1f6e8f6af0dde95d76458909c9116f7641202042
Olesia Shtanko <olesia.shtanko.21@um.edu.mt>
2023-05-26 02:09:17 +0200

    help dialog layout

--- app/src/main/java/com/example/peckishv2/Backend/MainApp.java
+++ app/src/main/java/com/example/peckishv2/Backend/MainApp.java
@@ -22,16 +22,17 @@ import com.example.peckishv2.databinding.ActivityMainAppBinding;
 public class MainApp extends AppCompatActivity {
 
     ActivityMainAppBinding binding;
-    ImageButton user_profile, preferences, general_settings, about_us, change_profile, logout;
+    ImageButton user_profile, preferences, about_us, change_profile, logout, help_btn;
     DrawerLayout drawerLayout;
     Toolbar tool_bar;
-    Dialog general_settings_dialog, about_us_dialog, change_profile_dialog;
+    Dialog about_us_dialog, change_profile_dialog, help_dialog;
 
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //binding xml to java
         binding = ActivityMainAppBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new HomePage());// on initial load, home page is loaded first
@@ -59,11 +60,10 @@ public class MainApp extends AppCompatActivity {
         tool_bar = findViewById(R.id.toolbar);
         user_profile = findViewById(R.id.profile);
         preferences = findViewById(R.id.user_preference);
-        general_settings = findViewById(R.id.general_settings);
         about_us = findViewById(R.id.about_us);
         change_profile = findViewById(R.id.change_profile);
         logout = findViewById(R.id.log_out);
-        change_profile = findViewById(R.id.change_profile);
+        help_btn = findViewById(R.id.help_btn);
 
         //setting up the custom tool bar
         setSupportActionBar(tool_bar);
@@ -71,12 +71,18 @@ public class MainApp extends AppCompatActivity {
         //open drawer
         user_profile.setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
 
-        //options
+        //options actions
         change_profile.setOnClickListener(v -> change_profile_dialog.show());
         preferences.setOnClickListener(v -> startActivity(new Intent(MainApp.this, Preferences.class)));
-        general_settings.setOnClickListener(v -> general_settings_dialog.show());
         about_us.setOnClickListener(v -> about_us_dialog.show());
         logout.setOnClickListener(v -> startActivity(new Intent(MainApp.this, Login.class)));
+        help_btn.setOnClickListener(v -> help_dialog.show());
+
+        //help dialog
+        help_dialog = new Dialog(this);
+        help_dialog.setContentView(R.layout.dialog_help);
+        help_dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.dialog_box));
+        help_dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
         //edit profile dialog
         change_profile_dialog = new Dialog(this);
@@ -85,18 +91,13 @@ public class MainApp extends AppCompatActivity {
         change_profile_dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
 
-        //general settings dialog
-        general_settings_dialog = new Dialog(this);
-        general_settings_dialog.setContentView(R.layout.dialog_general_settings);
-        general_settings_dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.dialog_box_mainapp));
-        general_settings_dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
         //about us dialog
         about_us_dialog = new Dialog(this);
         about_us_dialog.setContentView(R.layout.dialog_about_us);
         about_us_dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.dialog_box_mainapp));
         about_us_dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     }
+
     //closes in case of pressing on back arrow of the system
     @Override
     public void onBackPressed() {
